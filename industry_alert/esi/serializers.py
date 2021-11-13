ecefbb13e6f8210cac6e8067e1cc9cd6c6282831
Your Name <you@example.com>
2021-11-13 22:48:16 +0900

    1

--- industry_alert/esi/serializers.py
+++ industry_alert/esi/serializers.py
@@ -6,6 +6,6 @@ class IndustryJobsSerializer(serializers.ModelSerializer):
     class Meta:
         model = IndustryJobs
         fields = ['activity_id', 'blueprint_id', 'blueprint_location_id', 'blueprint_type_id', 'cost',
-                  'duration', 'end_date', 'facility_date', 'installer_id', 'job_id', 'licensed_runs', 'output_location_id',
+                  'duration', 'end_date', 'facility_id', 'installer_id', 'job_id', 'licensed_runs', 'output_location_id',
                   'probability', 'product_type_id', 'runs', 'start_date', 'station_id', 'status']
     # create overide해서 bulk create/update로 생성하기
