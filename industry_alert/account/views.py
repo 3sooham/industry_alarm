bdf863469dea40634b4878b20795457358248669
Your Name <you@example.com>
2021-11-20 17:51:34 +0900

    1

--- industry_alert/account/views.py
+++ industry_alert/account/views.py
@@ -161,7 +161,8 @@ class EveLoginViewSet(viewsets.GenericViewSet):
             print(type(i))
 
         li = [ins for ins in instance]
-        serializer = IndustryJobSerializer(instance, data=industry_jobs, many=True)
+        print(li)
+        serializer = IndustryJobSerializer(li, data=industry_jobs, many=True)
         # 유저가 처음 로그인해서 job이 비어있는 경우
         if user_created:
             try:
