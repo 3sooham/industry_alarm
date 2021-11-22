214aa0686337302547638c066b44bbe650097ab1
Your Name <you@example.com>
2021-11-23 01:30:52 +0900

    1

--- industry_alert/esi/serializers.py
+++ industry_alert/esi/serializers.py
@@ -18,33 +18,44 @@ class IndustryJobListSerializer(serializers.ListSerializer):
         print(type(IndustryJob(**validated_data[0])))
 
         # 유저에 대해서 저장된 잡이 있는지 확인
+        # filter에서는 db hit 안함
+        # 이거 찍어보면 queryset 오브젝트 나오고 하나 get하기전에는 db에서 값을 가져오지 않음
+        # https://docs.djangoproject.com/en/3.2/ref/models/querysets/
+        # evaluate할때만 db랑 통신함
         instance = IndustryJob.objects.filter(user=validated_data[0]['user'])
+        print(type(instance))
         # 유저에 대해서 저장된 잡이 있으면
         if instance.exists():
             # 기존에 존재하는 job들을 job_id를 key로 정리
+            # 여기서 iterate하니  db hit 하는거임
             job_mapping = {job.job_id: job for job in instance}
+            for j in job_mapping:
+                print(type(j))
+                break
             # 새로 받아온 job들을 job_id를 키로 정리
             data_mapping = {item['job_id']: item for item in validated_data}
 
+            ret = []
             # 새로 받아온 job이 db에 저장되어 있는지 확인하고 없으면 생성
             need_create = [
                 IndustryJob(**data) for job_id, data in data_mapping.items() if job_mapping.get(job_id) is None
             ]
             # 이거 []들어가면 실행안하고 끝나서 if need_crate: 이렇게 안해도됨
-            IndustryJob.objects.bulk_create(need_create)
+            ret.append(IndustryJob.objects.bulk_create(need_create))
 
             # 새로 받아온 job이 db에 저장되어 있고 status가 변경 됐으면 update 해줌
             need_update = [
-                IndustryJob(**data) for job_id, data in data_mapping.items() if job_mapping.get(job_id) is not None
+                IndustryJob(**data) for job_id, data in data_mapping.items()
+                if job_mapping.get(job_id) is not None and job_mapping.get(job_id).status != data['status']
             ]
-            IndustryJob.objects.bulk_update(need_update, ['status'])
+            ret.append(IndustryJob.objects.bulk_update(need_update, ['status']))
 
             # 이미 있는 잡이 새로 불러온 job에 없으면 완료되서 사라진거니 삭제해줌
             # 이거 python gc가 reference기반이어서 아래에서 더이상 안쓰면 알아서 지워줌
             # 아래 리스트 만들면서 job.delete() 실행하고 다음줄 내려가면서 메모리에서 날아감
             [job.delete() for job_id, job in job_mapping.items() if job_id not in data_mapping]
 
-            return need_create + need_update
+            return ret
 
         # 유저에 대해서 잡이 없으면 create
         industry_jobs = [IndustryJob(**item) for item in validated_data]
