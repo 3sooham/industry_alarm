674b914835f831fa7d3ce82aed6cb6ded94132c3
3sooham <gkr0404@naver.com>
2021-11-24 16:31:00 +0000

    1

--- industry_alert/mysite/settings.py
+++ industry_alert/mysite/settings.py
@@ -159,7 +159,7 @@ USE_L10N = True
 USE_TZ = False
 
 # django-celery-beat
-DJANGO_CELERY_BEAT_TZ_AWARE = False
+# DJANGO_CELERY_BEAT_TZ_AWARE = False
 
 # Static files (CSS, JavaScript, Images)
 # https://docs.djangoproject.com/en/3.2/howto/static-files/
