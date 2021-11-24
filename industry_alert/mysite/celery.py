3a8f5cc31742b07273804ebcdee9f6bc72bbcc43
Your Name <you@example.com>
2021-11-24 22:58:25 +0900

    2

--- industry_alert/mysite/celery.py
+++ industry_alert/mysite/celery.py
@@ -1,7 +1,6 @@
 import os
 
 from celery import Celery
-from account.tasks import temp_task
 
 # Set the default Django settings module for the 'celery' program.
 os.environ.setdefault('DJANGO_SETTINGS_MODULE', 'mysite.settings')
@@ -19,15 +18,13 @@ app.autodiscover_tasks()
 
 # on_after_finalize
 
-# @app.on_after_configure.connect
-
-@app.on_after_finalize
+@app.on_after_configure.connect
 def setup_periodic_tasks(sender, **kwargs):
     # Calls test('hello') every 10 seconds.
 
     # Setting these up from within the on_after_configure handler means that
     # we’ll not evaluate the app at module level when using test.s()
-    sender.add_periodic_task(10.0, temp_task.s(1, 2), name='add every 10')
+    sender.add_periodic_task(10.0, test.s(1, 2), name='add every 10')
 
 
 @app.task
