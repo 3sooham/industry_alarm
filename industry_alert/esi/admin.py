8ef414e7776fe054dba4bd6d40f82931ee42367c
Your Name <you@example.com>
2022-03-27 14:14:17 +0900

    added esi.Faclity model to admin

--- industry_alert/esi/admin.py
+++ industry_alert/esi/admin.py
@@ -1,5 +1,6 @@
 from django.contrib import admin
-from .models import IndustryJob
+from .models import IndustryJob, Facility
 # Register your models here.
 
 admin.site.register(IndustryJob)
+admin.site.register(Facility)
