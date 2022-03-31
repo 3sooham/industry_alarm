60e60c12e302b4297b381edd01e293a915996e10
Your Name <you@example.com>
2022-04-01 03:03:26 +0900

    3

--- industry_alert/esi/serializers.py
+++ industry_alert/esi/serializers.py
@@ -122,9 +122,9 @@ class IndustryJobSerializer(serializers.ModelSerializer):
         list_serializer_class = IndustryJobListSerializer
         model = IndustryJob
         # 이거 context로 user넣어줄거라서 user일단 fields에서 뺌
-        fields = ['id', 'activity_id', 'blueprint_id', 'blueprint_location_id', 'blueprint_type_id',
+        fields = ['id', 'activity_id', 'blueprint_id', 'blueprint',
                   'completed_character_id', 'completed_date', 'cost',
-                  'duration', 'end_date', 'facility', 'installer_id', 'job_id', 'licensed_runs', 'output_location_id',
+                  'duration', 'end_date', 'facility', 'installer_id', 'job_id', 'licensed_runs',
                   'pause_date', 'probability', 'product_type_id', 'runs', 'start_date', 'station_id', 'status',
                   'successful_runs']
 
